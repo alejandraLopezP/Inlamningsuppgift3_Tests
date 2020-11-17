a741213a4fd45ba4d54c9c7e02218fab0bfbdf54
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-17 20:18:25 +0100

    Implement console calculator

--- ConsoleCalc/Program.cs
+++ ConsoleCalc/Program.cs
@@ -23,17 +23,15 @@ namespace ConsoleCalc
                 input = Console.ReadLine();
                 commands = input.Split(' ');
 
-                if ( commands[0] == "quit")
-                {//
-                    Console.WriteLine("hej då");
+                if (commands[0] == "quit")
+                {
+                    Console.WriteLine("Bye!");
                     stop = true;
                 }
-                else if ( commands[0] == "enter" && commands.Length == 2 )
+                else if (commands[0] == "enter" && commands.Length == 2)
                 {
                     cs.entry = commands[1];
                     cs.Enter();
-                    Console.WriteLine("test");
-
                 }
                 else if (commands[0] == "+")
                 {
@@ -51,23 +49,90 @@ namespace ConsoleCalc
                 {
                     cs.BinOp("÷");
                 }
+                else if (commands[0] == "/")
+                {
+                    cs.BinOp("÷");
+                }
+                else if (commands[0] == "^")
+                {
+                    cs.BinOp("yˣ");
+                }
+                else if (commands[0] == @"\")
+                {
+                    cs.BinOp("ˣ√y");
+                }
+                else if (commands[0] == "sqr")
+                {
+                    cs.Unop("x²");
+                }
+                else if (commands[0] == "sqrt")
+                {
+                    cs.Unop("√x");
+                }
+                else if (commands[0] == "log")
+                {
+                    cs.Unop("log x");
+                }
+                else if (commands[0] == "ln")
+                {
+                    cs.Unop("ln x");
+                }
+                else if (commands[0] == "10^")
+                {
+                    cs.Unop("10ˣ");
+                }
+                else if (commands[0] == "e^")
+                {
+                    cs.Unop("eˣ");
+                }
+                else if (commands[0] == "sin")
+                {
+                    cs.Unop("sin");
+                }
+                else if (commands[0] == "cos")
+                {
+                    cs.Unop("cos");
+                }
+                else if (commands[0] == "tan")
+                {
+                    cs.Unop("tan");
+                }
+                else if (commands[0] == "asin")
+                {
+                    cs.Unop("sin⁻¹");
+                }
+                else if (commands[0] == "acos")
+                {
+                    cs.Unop("cos⁻¹");
+                }
+                else if (commands[0] == "atan")
+                {
+                    cs.Unop("tan⁻¹");
+                }
+                else if (commands[0] == "pi")
+                {
+                    cs.Nilop("π");
+                }
+                else if (commands[0] == "e")
+                {
+                    cs.Nilop("e");
+                }
                 else if (commands[0] == "show")
                 {
-                    Console.WriteLine($"T: {cs.T.ToString()}");
-                    Console.WriteLine($"Z: {cs.Z.ToString()}");
-                    Console.WriteLine($"Y: {cs.Y.ToString()}");
-                    Console.WriteLine($"X: {cs.X.ToString()}");
+                    Show(cs);
                 }
                 else
                 {
                     Console.WriteLine("wrong input, try again");
                 }
-
             } while (!stop);
-
-
             Console.ReadKey();
-
+        }
+        static void Show(CStack cs)
+        {
+            Console.WriteLine("T: {0}\nZ: {1}\nY: {2}\nX: {3}",
+                              cs.T.ToString(), cs.Z.ToString(),
+                              cs.Y.ToString(), cs.X.ToString());
         }
     }
 }
