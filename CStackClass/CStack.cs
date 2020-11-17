3e47281ff41e40d2fb46ccaf6cacfc3dbb565048
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-17 05:32:47 +0100

    Added Projects CStackClass, and dependent ConsoleCalc and WPFCalc

---
+++ CStackClass/CStack.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CStackClass
+{
+    public class CStack
+    {
+        public double X, Y, Z, T;
+    }
+}
---
+++ ConsoleCalc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CStackClass;
+
+namespace ConsoleCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
